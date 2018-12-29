ba34f767d7d24017976c95efacde26a7855289d6
test <test1>
2018-12-29 15:54:20 +0900

    갤러리에서 받아온 이미지 처리

--- app/src/main/java/tjeit/kr/startactivityforresulttest/MainActivity.java
+++ app/src/main/java/tjeit/kr/startactivityforresulttest/MainActivity.java
@@ -1,22 +1,30 @@
 package tjeit.kr.startactivityforresulttest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends BaseActivity {
 
     final static int REQUEST_FOR_USER_NAME = 1000;
     final static int REQUEST_FOR_USER_BIRTHDAY = 1001;
+    final static int REQUEST_FOR_PICTURE_GALLERY = 1002;
 
     private android.widget.TextView nameTxt;
     private android.widget.Button nameInputBtn;
     private TextView birthDayTxt;
     private Button birthDayBtn;
+    private android.widget.ImageView profileImg;
+    private Button cameraBtn;
+    private Button gallerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,14 @@ public class MainActivity extends BaseActivity {
         else if (requestCode == REQUEST_FOR_USER_BIRTHDAY) {
             Toast.makeText(mContext, "생년월일", Toast.LENGTH_SHORT).show();
         }
+        else if(requestCode == REQUEST_FOR_PICTURE_GALLERY){
+            if(resultCode == RESULT_OK){
+                Uri selectedImageUri = data.getData();
+//                String ImagePath = selectedImageUri.getPath();
+
+                Glide.with(mContext).load(selectedImageUri).into(profileImg);
+            }
+        }
 
     }
 
@@ -69,6 +85,22 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        gallerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_FOR_PICTURE_GALLERY);
+            }
+        });
     }
 
     @Override
@@ -79,6 +111,9 @@ public class MainActivity extends BaseActivity {
     @Override
     public void bindViews() {
 
+        this.gallerBtn = (Button) findViewById(R.id.gallerBtn);
+        this.cameraBtn = (Button) findViewById(R.id.cameraBtn);
+        this.profileImg = (ImageView) findViewById(R.id.profileImg);
         this.birthDayBtn = (Button) findViewById(R.id.birthDayBtn);
         this.birthDayTxt = (TextView) findViewById(R.id.birthDayTxt);
         this.nameInputBtn = (Button) findViewById(R.id.nameInputBtn);
